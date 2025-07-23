09122b758ad2e8606683dd5367a9dec4b6201fef
andredev <andluccag@gmail.com>
2025-07-23 15:29:02 -0300

    fix bug

--- src/components/Papel.jsx
+++ src/components/Papel.jsx
@@ -1,13 +1,14 @@
 import Wallpp from "../assets/Wallpaper.png"
 import Degrade from "../assets/degrade.png"
 import Bigwpp from "../assets/Wppbig.png"
+import degradebig from "../assets/degradebig.png"
 function Papel(){
     return(
         <>
             <div className="">
                 <img src={Wallpp} className="lg:hidden absolute z-10 top-0"/>
-                <img src={Degrade} className="absolute lg:hidden md:hiddem sm:hidden z-20 top-[-10px] lg:top-0 w-full max-w-full"/>
-                <img src={Bigwpp} className="hidden lg:block w-full  absolute z-10 top-0"/>
+                <img src={Degrade} className=" absolute lg:hidden z-20 top[10px] w-full max-w-full"/>
+                <img src={degradebig} className="hidden lg:block w-full  h-300% absolute z-10 top-0"/>
             </div>
         </>
     )
